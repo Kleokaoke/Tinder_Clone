d210e257d4f29839a0cd39ba46ac84baa6915f6d
unknown <kleokaoke7@gmail.com>
2020-08-06 14:51:34 +0200

    changes to image . java

--- src/Image.java
+++ src/Image.java
@@ -1,9 +1,60 @@
 public class Image {
 
     // Fields:
+    
+
+        String url ;
+        String description ;
+        String title ;
+        String tags ;  // Array
+    
+
+
 
     // Constructor:
-    Image () {
+
+    Image(String url ,String description , String title, String tags){
+
+        this.url = url ;
+        this.desctription = description ;
+        this.title = title ;
+        this.tags = tags ;
 
     }
+    
+       
+    // Setters
+
+        public void seturl(String url) {
+            this.url = url;
+        }
+        public void set description(String description) {
+            this.description = description;
+        }
+        public void settitle(String title) {
+             this.title = title;
+        }
+        public void settags(String tags) {
+             this.tags = tags;
+        }
+
+    // Getters 
+
+        public String geturl(){
+            return this.url;
+        }
+
+         public String getdescription(){
+             return this.description;
+        }
+
+        public String gettitle(){
+            return this.title;
+        }
+
+        public String gettags(){
+            return this.tags;
+        }
+
+
 }
