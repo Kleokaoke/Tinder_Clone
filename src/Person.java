8425d3d606cedaa441b9f01933b5e421def6eba1
LonwaboMvovo <44nator@gmail.com>
2020-08-06 11:54:05 +0200

    person class

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+
+    // Fields:
+    String name;
+    int age;
+    String gender;
+    String location;
+    String bio;
+
+    // Constructor:
+    Person(String name, int age, String gender, String location, String bio) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.location = location;
+        this.bio = bio;
+    }
+}
